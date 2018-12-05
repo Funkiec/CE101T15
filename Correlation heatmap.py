1df1e8570dc5f0a6c5a2698fb01bf1ae3eab1698
Funkiec <45242342+Funkiec@users.noreply.github.com>
2018-12-05 18:31:18 +0000

    Update Correlation heatmap.py

--- Correlation heatmap.py
+++ Correlation heatmap.py
@@ -17,3 +17,11 @@ df_train = pd.read_csv('../input/train.csv')
 corrmat = df_train.corr()
 f, ax = plt.subplots(figsize=(12, 9))
 sns.heatmap(corrmat, vmax=.8, square=True);
+
+#saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
