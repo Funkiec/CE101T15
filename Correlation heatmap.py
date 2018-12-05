0a3dfd0c47167bbfdb6d482483a333a5925c194f
Funkiec <45242342+Funkiec@users.noreply.github.com>
2018-12-05 18:20:20 +0000

    Add files via upload

---
+++ Correlation heatmap.py
@@ -0,0 +1,19 @@
+#invite people for the Kaggle party
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+import matplotlib.pyplot as plt
+
+df_train = pd.read_csv('../input/train.csv')
+
+
+#correlation matrix
+corrmat = df_train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True);
